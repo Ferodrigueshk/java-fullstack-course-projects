36f8d7f62b59034c9f30d42cddf9b1e3600a38a3
Fernando Rodrigues de Moura <Fernando.rodrigueshk@gmail.com>
2025-06-01 08:45:05 -0300

    Create operacoesBasicas.java

---
+++ estruturaSequencial/operacoesBasicas.java
@@ -0,0 +1,13 @@
+package estruturaSequencial;
+
+public class operacoesBasicas {
+
+    public static void main(String[] args) {
+        /*
+        Três operações basicas:
+        Entrada (leitura e armazenamento)
+        processamento (calculo e processamento)
+        saída (retorno e informação dos dados)
+         */
+    }
+}
