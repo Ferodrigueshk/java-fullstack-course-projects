bd3b4cefca65c321dd70ecd38104081c3fa90485
Fernando Rodrigues de Moura <Fernando.rodrigueshk@gmail.com>
2025-06-01 09:45:13 -0300

    Create exercicoFixacaoSaidaDados.java

---
+++ estruturaSequencial/exercicoFixacaoSaidaDados.java
@@ -0,0 +1,19 @@
+package estruturaSequencial;
+
+public class exercicoFixacaoSaidaDados {
+    public static void main(String[] args) {
+        /*
+         * Em seguida, usando os valores das variáveis, produza a
+         * seguinte saída na tela do console:
+         * Products:
+         * Computer, which price is $ 2100,00
+         * Office desk, which price is $ 650,50
+         * Record: 30 years old, code 5290 and gender: F
+         * Measue with eight decimal places: 53,23456700
+         * Rouded (three decimal places): 53,235
+         * US decimal point: 53.235
+         */
+
+    }
+
+}
