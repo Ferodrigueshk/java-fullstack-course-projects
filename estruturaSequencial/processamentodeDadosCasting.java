7d37d274e5df4fa321caa0aa19bcea1f6675fcae
Fernando Rodrigues de Moura <Fernando.rodrigueshk@gmail.com>
2025-06-04 20:56:18 -0300

    Update processamentodeDadosCasting.java

--- estruturaSequencial/processamentodeDadosCasting.java
+++ estruturaSequencial/processamentodeDadosCasting.java
@@ -8,7 +8,44 @@ public class processamentodeDadosCasting {
         y = 2 * x;
         System.out.println(x);
         System.out.println(y);
+        System.out.println("--- ");
 
+        /* __________________Parte B da Aula_________________________ */
+        System.out.println("--- ");
+        double b, B, h, area;
+        b = 6;
+        B = 8;
+        h = 5;
+
+        area = (b + B) / 2 * h;
+        System.out.println(b);
+        System.out.println(B);
+        System.out.println(h);
+        System.out.println(area);
+        System.out.println("--- ");
+
+        /* __________________Parte C da Aula_________________________ */
+
+        float baseMenor, baseMaior, altura, areaTrapezio;
+        baseMenor = 6f;
+        baseMaior = 8f;
+        altura = 5f;
+
+        areaTrapezio = (baseMenor + baseMaior) / 2 * altura;
+        System.out.println(baseMenor);
+        System.out.println(baseMaior);
+        System.out.println(altura);
+        System.out.println(areaTrapezio);
+
+        System.out.println("--- ");
+        /* __________________Parte D da Aula_________________________ */
+
+        int num1, num2;
+        double resultado2;
+        num1 = 5;
+        num2 = 2;
+        resultado2 = num1 / num2;
+        System.out.println(resultado2);
     }
 
 }
