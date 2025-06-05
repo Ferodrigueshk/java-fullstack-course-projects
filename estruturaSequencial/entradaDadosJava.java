b10903b62827afc5281cb3f8bffe524e2a48308c
Fernando Rodrigues de Moura <Fernando.rodrigueshk@gmail.com>
2025-06-05 13:34:36 -0300

    Update entradaDadosJava.java

--- estruturaSequencial/entradaDadosJava.java
+++ estruturaSequencial/entradaDadosJava.java
@@ -9,18 +9,24 @@ public class entradaDadosJava {
         String nome = scan.nextLine();
         System.out.println("Seu nome é: " + nome);
 
-        /* ------------- Armazenando dados com ponto flutuante ---------------- */
+        /*
+         * ------------- Armazenando dados com ponto flutuante ----------------
+         */
         System.out.println("Digite seu salario: ");
         double salario = scan.nextDouble();
         System.out.printf("Seu salario é: R$ %.2f%n ", salario);
         System.out.printf("Você digitou: %.2f%n", salario);
 
-        /* ------------- Leitura de apenas um caractere ---------------- */
+        /*
+         * ------------- Leitura de apenas um caractere ----------------
+         */
         System.out.println("Sexo: (M/F)");
         char sexo = scan.next().charAt(0);
         System.out.println("Sexo: " + sexo);
 
-        /* ------------- Para ler vários dados na mesma linha ---------------- */
+        /*
+         * ------------- Para ler vários dados na mesma linha ----------------
+         */
         String a1;
         int a2;
         double a3;
@@ -30,7 +36,22 @@ public class entradaDadosJava {
         a3 = scan.nextDouble();
         System.out.println(a1 + " " + a2 + " " + a3);
 
-        /* ------------- Continuaão da aula ---------------- */
+        /*
+         * ------------- Continuação da aula ----------------
+         */
+
+        /*
+         * ------------- Lendo um texto até a primeira quebra de linha ---------------
+         */
+
+        String s1, s2, s3;
+        s1 = scan.nextLine();
+        s2 = scan.nextLine();
+        s3 = scan.nextLine();
+        System.out.println("Dados digitados: ");
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
 
         scan.close();
     }
